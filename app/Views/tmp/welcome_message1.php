ca97febc848fa2b14671494032508a03c1358b55
Denis <seereserv@mail.ru>
2022-02-03 17:46:50 +0500

    first commit

---
+++ app/Views/tmp/welcome_message1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo ASSETS_PATH?>/css/style1.css" />
+
+</head>
+<body>
+<!--D:\OpenServer\OpenServer\domains\edit_table\et3\app\Views\tmp\style.css-->
+<span class="test_class">welcome_message1</span>
+<span class="test_class">welcome_message1</span>
+<br>
+<a href="https://codeigniter.com/user_guide/models/model.html">https://codeigniter.com/user_guide/models/model.html</a>
+<br>
+<a href="https://bootstrap-4.ru/docs/5.1/customize/components/">https://bootstrap-4.ru/docs/5.1/customize/components/</a>
+
+<br>
+site_url() - <?php echo site_url('/style.css'); ?><br>
+index_page() - <?php echo index_page(); ?><br>
+base_url() - <?php echo base_url(); ?><br>
+current_url() - <?php echo current_url(); ?><br>
+<?php echo $q1; ?>
+<br>
+<?php
+//    foreach ($q2 as $key => $val){
+//        echo $key . '--' . $val . '<br>';
+//    }
+?>
+
+</body>
+</html>
